d515bc953283574fdccd87812fc230cd54794415
TheGeeKing <58857539+TheGeeKing@users.noreply.github.com>
2022-07-20 20:35:59 +0200

    Discord RPC Maker v2.0
    Added:
    - You can now Save/Delete presets!
    - Added to some variable their explicit type.
    
    Changed:
    - Presets dropdown menu is now readonly and has a tooltip.
    - Window is now directly finalize to update presets after.
    - Window is closed and then all processes are closed.
    - Changed the way popup from the queue are handled
    
    Fixed:
    - Quotes in input fields were raising error due to wrong handling when creating parameters for RPC.

--- discord_rpc_maker.py
+++ discord_rpc_maker.py
@@ -1,4 +1,6 @@
 import asyncio
+import contextlib
+import json
 import os
 import sys
 import threading
@@ -16,7 +18,30 @@ from validators import url
 if getattr(sys, 'frozen', False): # Running as compiled
     chdir(sys._MEIPASS) # change current working directory to sys._MEIPASS
 
-def ram_cpu():
+class QueueEvents():
+    """
+    This class is a helper class that will be used to pass PySimpleGUI events that need to be run in main thread.
+
+    The class has two methods:
+
+    1. append: This method will append the events that need to be run in main thread.
+    2. pop: This method will return the first event in the queue to be executed and removes it from the queue.
+    """
+
+    def __init__(self):
+        self.queue = []
+
+    def append(self, other) -> list:
+        self.queue.append(other)
+        return self.queue
+
+    def pop(self) -> str:
+        if self.queue:
+            return self.queue.pop(0)
+
+queue = QueueEvents()
+
+def ram_cpu() -> tuple[str, str]:
     """
     Get CPU and RAM usage and return them as a tuple
     :return: A tuple of strings.
@@ -75,12 +100,12 @@ def set_rpc(app_id): #TODO: make it compatible with presets
             elif key == "Party Size: < int > , < int > " and values[value] != "" and values['-STATE-'] == "":
                 queue.append("""sg.popup_error("State needs to be specified if Party Size is specified!", title="Party Size and State")""")
             elif key == "Party Size: < int > , < int > " and values[value] != "":
-                v = values[value].split(", ")
+                v:tuple[int, int] = values[value].split(", ")
                 if len(v) != 2:
                     queue.append("""sg.popup_error("Party Size needs to be in the format of < int > , < int > ", title="Invalid Party Size format.")""")
                     continue
                 try:
-                    v = (int(v[0]), int(v[1]))
+                    v:tuple[int, int] = (int(v[0]), int(v[1]))
                 except ValueError:
                     queue.append("""sg.popup_error("Party Size needs to be in the format of < int > , < int > ", title="Invalid Party Size format.")""")
                 if min(v) > 0:
@@ -94,7 +119,7 @@ def set_rpc(app_id): #TODO: make it compatible with presets
                     queue.append(f"""sg.popup_error("The text must be at least 2 characters long.", title="{key}")""")
             else:
                 fields_clean[table[key]] = values[value]
-        params = ", ".join(f"{key} = '{value}'" if key != "party_size" else f"party_size={fields_clean['party_size']}" for key, value in fields_clean.items())
+        params = ", ".join(f"{key}=\"{value}\"" if key != "party_size" else f"party_size={fields_clean['party_size']}" for key, value in fields_clean.items())
         if params != "":
             if buttons != []:
                 params = f"{params}, buttons={buttons}"
@@ -158,10 +183,9 @@ def set_rpc_current_window(app_id):
         ram = parent_process.memory_full_info().uss
         for child in parent_process.children(recursive=True):
             with child.oneshot():
-                try:
+                with contextlib.suppress(Exception):
                     cpu += child.cpu_percent(0.1) / psutil.cpu_count()
                     ram += child.memory_full_info().uss
-                except:pass
         ram = ram / (1024 ** 2)
 
         RPC.update(details=window, state=f"cpu: {cpu:.2f}% | ram: {ram:.0f}MB/{psutil.virtual_memory().total / (1024 ** 2):.0f}MB")
@@ -186,12 +210,6 @@ fields = {
     "Button 2 URL:": {"key": "-BUTTON2_URL-", "tooltip": "The URL of the second button."}
 }
 
-presets = {
-    "": "",
-    "RAM/CPU": set_rpc_ram_cpu,
-    "Current Activity": set_rpc_current_window
-}
-
 sg.theme('DarkAmber')
 layout = [[sg.Text("* fields are mandatory | RPC is updated every 15 seconds (ratelimit)")]]
 
@@ -201,49 +219,68 @@ layout.append(
     [
         [
             sg.Text('Presets:'),
-            sg.Combo(tuple(presets), key='-PRESETS-', enable_events=True)
+            sg.Combo("", key='-PRESETS-', enable_events=True, readonly=True, size=15, tooltip='Select a preset'),
+            sg.Push(),
+            sg.Input("", key="-INPUT-PRESET-", size=(20, 1), tooltip="Enter a preset name to save it."),
+            sg.Button("Save", key="-SAVE-", button_color="lightblue"),
+            sg.Button("Delete", key="-DELETE-", button_color="red")
         ],
         [
             sg.Button("ON", key="-SWITCH-", button_color="green"),
-            sg.Button("Clear", key="-CLEAR-", button_color="grey"),
+            sg.Button("Clear", key="-CLEAR-", button_color="grey")
         ],
     ]
 )
 
 
-window = sg.Window('Discord RPC Maker', layout=layout, icon="MMA.ico")
+window = sg.Window('Discord RPC Maker', layout=layout, icon="MMA.ico", finalize=True)
 
+config_format = list(fields)
+elements_key = [value["key"] for value in fields.values()]
 
-class QueueEvents():
-    """
-    This class is a helper class that will be used to pass PySimpleGUI events that need to be run in main thread.
+def build_presets_dropdown():
+    presets = {
+    "": "",
+    "RAM/CPU": set_rpc_ram_cpu,
+    "Current Activity": set_rpc_current_window
+    }
 
-    The class has two methods:
+    config_filename = []
 
-    1. append: This method will append the events that need to be run in main thread.
-    2. pop: This method will return the first event in the queue to be executed and removes it from the queue.
-    """
+    for file_ in os.listdir('config'):
+        if file_.endswith('.json') and os.path.isfile(os.path.join('config', file_)):
+            # verify that the file is valid and has the key in the config_format
+            with open(f'config/{file_}', 'r') as f:
+                try:
+                    data = json.load(f)
+                    if all(key in data for key in config_format):
+                        config_filename.append(file_)
+                    else:
+                        x = '\n'.join(config_format) # due to the fact that: SyntaxError: f-string expression part cannot include a backslash
+                        queue.append(f"{file_} is not a valid config file.\nFormat:\n{x}\nare required even if empty.")
+                except Exception as e:
+                    queue.append(f"{file_} is not a valid config file. It is not a valid JSON file.\n{e}")
 
-    def __init__(self):
-        self.queue = []
+    for config in config_filename:
+        presets[config[:-5]] = set_rpc
 
-    def append(self, other) -> list:
-        self.queue.append(other)
-        return self.queue
+    return presets
 
-    def pop(self) -> str:
-        if self.queue:
-            return self.queue.pop(0)
+def update_presets_dropdown():
+    presets = build_presets_dropdown()
+    window['-PRESETS-'].update(values=tuple(presets))
+    return presets
 
-queue = QueueEvents()
+presets = update_presets_dropdown()
 
 if __name__ == "__main__":
     while True:
         event, values = window.Read(timeout=100)
         if event is None or event in [sg.WIN_CLOSED]: #TODO: detect if thread is running and set thread.do_run = False
+            window.close()
             os._exit(0)
         elif queue.queue:
-            exec(queue.pop())
+            sg.popup_error(queue.pop(), title="Error")
         elif event == "-SWITCH-" and values["-APP_ID-"].isdecimal() and len(values["-APP_ID-"]) == 18 and window["-SWITCH-"].ButtonText == 'ON' and values["-PRESETS-"] == "":
             thread = threading.Thread(target=set_rpc, args=(values["-APP_ID-"], ))
             thread.do_run = True
@@ -259,11 +296,17 @@ if __name__ == "__main__":
             thread = threading.Thread(target=set_rpc, args=(values["-APP_ID-"], ))
             thread.do_run = True
             thread.start()
-        elif event == "-PRESETS-" and values["-APP_ID-"].isdecimal() and len(values["-APP_ID-"]) == 18 and window["-SWITCH-"].ButtonText == 'OFF' and values["-PRESETS-"] != "":
+        elif event == "-PRESETS-" and values["-APP_ID-"].isdecimal() and len(values["-APP_ID-"]) == 18 and window["-SWITCH-"].ButtonText == 'OFF' and values["-PRESETS-"] != "" and presets[values["-PRESETS-"]] != set_rpc:
             thread.do_run = False
             thread = threading.Thread(target=presets[values["-PRESETS-"]], args=(values["-APP_ID-"], ))
             thread.do_run = True
             thread.start()
+        elif event == "-PRESETS-" and window["-SWITCH-"].ButtonText == 'ON' and values["-PRESETS-"] != "" and presets[values["-PRESETS-"]] == set_rpc:
+            with open(f'config/{values["-PRESETS-"]}.json', 'r', encoding='utf-8') as f:
+                data = json.load(f)
+            for key, key_ in zip(elements_key, data.keys()):
+                window[key].Update(data[key_])
+            window["-PRESETS-"].Update(data["Presets"])
         elif event == "-SWITCH-" and window["-SWITCH-"].ButtonText == 'ON' and not values["-APP_ID-"].isdecimal() and len(values["-APP_ID-"]) != 18:
             response = sg.popup_yes_no("Application ID must be 18 digits long. Would you like to be redirected to the discord webpage to get your application ID?")
             if response == "Yes":
@@ -275,4 +318,35 @@ if __name__ == "__main__":
             for element in values:
                 if element not in ["-APP_ID-"]:
                     window[element].Update('')
-    window.close()
+        elif event == "-SAVE-":
+            if values["-INPUT-PRESET-"] == "Example":
+                sg.popup_ok("You can't save a preset with the name 'Example'.")
+            elif values["-INPUT-PRESET-"] != "":
+                try:
+                    v = {key: values[key_] for key, key_ in zip(config_format, elements_key)}
+                    v["Presets"] = values["-PRESETS-"]
+
+                    with open(f'config/{values["-INPUT-PRESET-"]}.json', 'w', encoding='utf-8') as f:
+                        json.dump(v, f, indent=2)
+
+                    sg.popup_ok("Preset saved!")
+                except Exception as e:
+                    sg.popup_error(f"{e}", title="Error")
+
+                presets = update_presets_dropdown()
+            else:
+                sg.popup_error("Please enter a preset name.", title="Error")
+        elif event == "-DELETE-":
+            if values["-INPUT-PRESET-"] == "Example":
+                sg.popup_ok("You cannot delete the preset 'Example'.")
+            elif values["-INPUT-PRESET-"] != "":
+                try:
+                    os.remove(f'config/{values["-INPUT-PRESET-"]}.json')
+                    sg.popup_ok("Successfully deleted preset.", title="Success")
+                except Exception as e:
+                    sg.popup_error(f"{e}", title="Error")
+                window["-INPUT-PRESET-"].Update("")
+
+                presets = update_presets_dropdown()
+            else:
+                sg.popup_error("Please enter a preset name.", title="Error")
